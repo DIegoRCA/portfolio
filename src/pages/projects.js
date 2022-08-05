457bbddcdd0a49cd58bf5a9b2759c62bb0b37ba0
DiegoRCA <diegocartelle@gmail.com>
2022-08-05 14:27:27 +1000

    projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,23 @@
+import React from "react";
+import { Container} from "react-bootstrap";
 
+function Projects() {
+	return (
+		<Container fluid className="project-section">
+			
+			<Container>
+				<h1 className="project-heading">
+					My Recent <strong className="purple">Works </strong>
+				</h1>
+				<p style={{ color: "white" }}>
+					Here are a few projects I've worked on recently.
+				</p>
+				</Container>
+		</Container>
+	);
+}
+
+export default Projects;
 
 
 
