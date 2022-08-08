4a90db1b2754d720b3df1a218a423c0142abb616
DiegoRCA <diegocartelle@gmail.com>
2022-08-08 13:01:06 +1000

    r

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -34,7 +34,7 @@ function ResumeNew() {
 							className="cardmedia"
 							component="iframe"
 							height="1056px"
-							src="https://drive.google.com/file/d/1-fQ5YAMpvM47YXmQNiF_L4KaQl1Kct0eLZKYIRMbXkA/preview"
+							src="https://drive.google.com/file/d/1ym8GjIGp8BN7w6nxxEvzACOGhw_l3AXD/preview"
 						/>
 					</Card>
 				</Row>
